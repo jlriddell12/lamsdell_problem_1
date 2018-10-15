f585f0872139931cfe05d7239ac588e0089099b7
Jill Riddell <jlriddell@mix.wvu.edu>
2018-10-15 11:09:59 -0400

    Changes to .R project file including changing filenames and eliminating previously unnecessary lines

--- lamsdell_project.R
+++ lamsdell_project.R
@@ -1,7 +1,8 @@
-#Download Lamsdell's file Matrices 461-470 into your working directory
-#rename the file Matrix_461-470 as bash and R don't like files with dashes or spaces in the file name
+#Download Lamsdell's file Matrices 461-470.xlsx into your working directory
+setwd("~/Desktop/ESDA/lamsdell_problem_1")
 install.packages(readxl)
 #install the readxl package
 library(readxl)
 #Call in the read xl package
-matrix_461_570 <- read_xlsx("Matrics_461_470")
+xfile_name <- "Matrices 461-470.xlsx"
+x_data <- read_xlsx(xfile_name)
