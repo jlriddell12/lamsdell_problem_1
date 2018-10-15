03c997b2547d39470a6c6b0a10e1e02e6a519b7d
autumdowney <ardowney@mix.wvu.edu>
2018-10-15 15:28:03 -0400

    made sheets vector

--- lamsdell_project.R
+++ lamsdell_project.R
@@ -1,8 +1,8 @@
 #Download Lamsdell's file Matrices 461-470.xlsx into your working directory
-setwd("~/Desktop/ESDA/lamsdell_problem_1")
 install.packages(readxl)
 #install the readxl package
 library(readxl)
 #Call in the read xl package
 xfile_name <- "Matrices 461-470.xlsx"
-x_data <- read_xlsx(xfile_name)
+x_data <- read_xls(xfile_name)
+sheets <- excel_sheets(xfile_name)
