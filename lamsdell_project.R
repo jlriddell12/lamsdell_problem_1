732a93a5f52a4e36ee2309c69533ace753608bbe
Jill Riddell <jlriddell@mix.wvu.edu>
2018-10-16 13:53:56 -0400

    Added install.packages("dplyr") and library("dplyr"), this is needed for R to call in the tbl functions in case future collaborators haven't installed the package.

--- lamsdell_project.R
+++ lamsdell_project.R
@@ -2,7 +2,8 @@
 install.packages("readxl")
 #install the readxl package
 library("readxl")
-#Call in the read xl package
+library("dplyr")
+#Call in the read xl and dplyr package
 xfile_name <- "Matrices 461-470.xlsx"
 x_data <- read_xlsx(xfile_name)
 sheets <- excel_sheets(xfile_name)
