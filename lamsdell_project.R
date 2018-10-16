3a0994500f578440992c6220cad41704c74f5cb5
Casey Lab <bnc0015@mix.wvu.edu>
2018-10-16 13:09:29 -0400

    Added "" around readxl in install (line 2) and library (line 4).  I have to have the quotes to run the command. Is this not necessary for you both?

--- lamsdell_project.R
+++ lamsdell_project.R
@@ -1,7 +1,7 @@
 #Download Lamsdell's file Matrices 461-470.xlsx into your working directory
-install.packages(readxl)
+install.packages("readxl")
 #install the readxl package
-library(readxl)
+library("readxl")
 #Call in the read xl package
 xfile_name <- "Matrices 461-470.xlsx"
 x_data <- read_xlsx(xfile_name)
