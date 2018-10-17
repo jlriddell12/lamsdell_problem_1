5458e337273ceef4cb3e1c0d89806a3538738909
Jill Riddell <jlriddell@mix.wvu.edu>
2018-10-17 14:02:58 -0400

    Made group a vector instead of data frame. Added comment to end of code about combining lines in future once we are sure the end code will work on all the sheets.

--- lamsdell_project.R
+++ lamsdell_project.R
@@ -23,10 +23,11 @@ species <- x_data[2:55,2]
 #vector of species names excluding ancestor row
 
 group_w <- fill(data_tb, 1, .direction = "down")
-group <- group_w [-56,]
+group <- group_w [-56,1]
 rm (group_w)
 #Fills in missing family values in first column, then excludes ancestor line and removes excess file
 
 column_number=3
 v1 <- x_data[2:56,column_number]
 #assign variable x as desired column number within the matrix, extracts selected column
+#Note that in the final assignment, some of these lines will likely be combined into single lines of code. Here, they are separated for clarity and to ensure the template will work for all the sheets.
