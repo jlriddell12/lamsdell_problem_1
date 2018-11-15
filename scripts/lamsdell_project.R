b1be581b8f1d673d3ffb8076fe77b1d51c13fdfe
Casey Lab <bnc0015@mix.wvu.edu>
2018-11-15 14:35:25 -0500

    fixed group vector to exclude the grand mean line. added to the loop to exclude ancestor line in output
    Both of these were done in preperation for creating a sinlge matrix of rowmeans

--- scripts/lamsdell_project.R
+++ scripts/lamsdell_project.R
@@ -27,27 +27,27 @@ sheets <- excel_sheets(xfile_name)
 #vector of species names excluding ancestor row
 species <- pull(x_data[2:55,2], var = 1)
 
-#Fills in missing family values in first column, then excludes ancestor line and removes excess file
-##I adjusted this to use your tibble called x_data
-group <- pull((fill(x_data, 1, .direction = "down")[1:(nrow(x_data)-1),]), var = 1)
+#Fills in missing family values in first column, then excludes first line and ancestor line and removes excess file
+group <- pull((fill(x_data, 1, .direction = "down")[2:(nrow(x_data)-1),]), var = 1)
 
 # Loops the recoding function through each matrix in a workbook, adds summatation column for each row, and mean for each group 
 for (sheet_name in sheets) {
   do
-  data_tb <- read_excel(xfile_name, sheet = sheet_name, range = "R4C3:R58C22", col_names = FALSE) #assigns selected excel file name, sheet, and range 
+  
+  data_tb <- read_excel(xfile_name, sheet = sheet_name, range = "R4C3:R58C22", col_names = FALSE) #assigns selected excel file name, sheet, and range
+  
   jl_vector <- pull(data_tb, X__1) #Extraction of first column in vector form
   
   source("functions/Lamsdell_Recoding_function.R") #sets the source for where the function is stored
   
   m <- apply(data_tb, 2, recoding_function) #calls the funtion and applies it to data_tb
   
-  m_sums <- cbind(group, m, sums=rowSums(m))#Sums the rows into a new column on the end of the recoded matrix 
-  
-  weighted <- cbind(m_sums, weighted=(rowSums(m)/20)) #Calculates the weighted average by sum/20
-  
-  assign(paste0(sheet_name, "d"), weighted) #Outputs each individual sheet produced through the loop  
+  assign(paste0(sheet_name, "d"), m[1:(nrow(m)-1),]) #Outputs each individual sheet produced through the loop, excludes ancestor line  
 }
 
+
+
 ## This works to get the mean of the species, but can not include in loop because of mismatched rows
+##m_mean <- cbind(group, m, sums=rowMeans(m))#Sums the rows into a new column on the end of the recoded matrix
 ##clade_mean <- as.data.frame(m_sums) %>% group_by(group) %>% summarise(mean = mean(as.numeric(sum)))
 ##AH- The clade mean and group means can be calculted outside the loop - see next assignment.
