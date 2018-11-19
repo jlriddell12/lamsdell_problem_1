7d7db80156e6260df4f4d6445bd4c64287dd1019
Jill Riddell <jlriddell@mix.wvu.edu>
2018-11-19 13:32:39 -0500

    Added code to assemble all summary mean columns in one sheet called summaryM. Also had to remove the done file because the loop was trying to include it.

--- jl_loop_loop.R
+++ jl_loop_loop.R
@@ -21,9 +21,29 @@ for (file_name in files){
     
     m <- apply(data_tb, 2, recoding_function) #calls the funtion and applies it to data_tb
     
-    m_mean <- cbind(m, mean=rowMeans(m)) #Mean of the species rows into a new column on the end of the recoded matrix
+    m_mean <- assign(paste(sheet_name), rowMeans(m)) #Mean of the species rows into a new column on the end of the recoded matrix
     
-    assign(paste0(sheet_name, "d"), m_mean[1:(nrow(m_mean)-1),]) #Outputs each individual sheet produced through the loop, excludes ancestor line
+    
+    stuff <- cbind(m, m_mean)
+    
+    #assign(paste0(sheet_name, "d"), stuff[1:(nrow(stuff)-1),])
+  }
+}
+    
+    
+    #assign(paste0(sheet_name, "d"), m_mean[1:(nrow(m_mean)-1),]) #Outputs each individual sheet produced through the loop, excludes ancestor line
     
   }
 }
+
+dataframe_list = grep("\\w+\\s\\w+d", ls(), value=TRUE) #makes a vector for the matrices. Might not need.
+
+summaryM <- matrix(nrow=55) #makes an empty data frame for means. Need to fill..?
+
+for (y in dataframe_list){
+  v <- rowMeans(get(y))
+  summaryM <- cbind(summaryM, v)
+}
+
+summaryM <- summaryM[,-1]
+colnames(summaryM) <- dataframe_list
