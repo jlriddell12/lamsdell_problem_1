c68243f6b65d0f65515b73f118eef87ea73ab0a4
Jill Riddell <jlriddell@mix.wvu.edu>
2018-11-19 19:23:08 -0500

    Made code a little neater; added some more explanation to annotations; deleted extra script.

--- scripts/lamsdell_project.R
+++ scripts/lamsdell_project.R
@@ -19,21 +19,14 @@ for (file_name in files){
   sheets <- excel_sheets(paste0("data/", file_name)) #creates a vector of sheet names
   species <- pull(x_data[2:55,2], var = 1) #vector of species names excluding ancestor row and first row (the "grand" score)
   group <- pull((fill(x_data, 1, .direction = "down")[2:(nrow(x_data)-1),]), var = 1) #Fills in missing family values in first column, then excludes first line and ancestor line and removes excess file
+  
   for (sheet_name in sheets) {
-    do
-    
     data_tb <- read_excel(paste0("data/", file_name), sheet = sheet_name, range = "R4C3:R58C22", col_names = FALSE) #assigns selected excel file name, sheet, and range
-    
     jl_vector <- pull(data_tb, X__1) #Extraction of first column in vector form
-    
     source("functions/Lamsdell_Recoding_function.R") #sets the source for where the function is stored
-    
     m <- apply(data_tb, 2, recoding_function) #calls the funtion and applies it to data_tb
-    
-    m_mean <- cbind(m, mean=rowMeans(m)) #Mean of the species rows into a new column on the end of the recoded matrix
-    
-    assign(paste0(sheet_name, "d"), m_mean[1:(nrow(m_mean)-1),]) #Outputs each individual sheet produced through the loop, excludes ancestor line
-    
+    m_mean <- cbind(m, mean=rowMeans(m)) #Mean of the species rows into a new column on the end of the recoded matrix. This isn't necessary for the assignment but could be useful in future projects if the user wanted individual matrix outputs with the mean
+    assign(paste0(sheet_name, "dmean"), m_mean[1:(nrow(m_mean)-1),]) #Outputs each individual sheet produced through the loop, excludes ancestor line 
     matrix_list <- ls(pattern="Matrix") #creates a vector of all matrices ran through the recoding loop
   }
 }
